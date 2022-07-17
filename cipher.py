2c61044542562e4960813f45059c3f9480ee5725
JJ Escandor <jj.p.escandor@gmail.com>
2022-07-17 13:20:41 -0400

    installed dependencies

---
+++ cipher.py
@@ -0,0 +1,17 @@
+from PyMultiDictionary import MultiDictionary
+
+
+def encrypt(str, shift):
+    pass
+
+
+def decrypt(str, shift):
+    pass
+
+
+def crack(str, shift):
+    pass
+
+
+if __name__ == "__main__":
+    pass
